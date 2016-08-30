8252974d70448dbd36ded918720b8582d125ed7f
alibhatti99 <alibhatti99@gmail.com>
2016-08-30 11:18:33 -0700

    Summary: Added a factory with several methods that get and update school list and it works!

--- client/app/schools/addSchools.js
+++ client/app/schools/addSchools.js
@@ -1,13 +1,36 @@
 angular.module('skool.addSchools', [])
-  .controller('addSchoolsController', function($scope, $state){
-    $scope.list = [];
-    $scope.schoolName = '';
-    $scope.submit = function() {
-      if ($scope.schoolName) {
-        $scope.list.push(this.schoolName);
-        $scope.schoolName = '';
-        $state.go('favorites');
+  .factory('schoolFactory', function(){
+    var service = {};
+
+    var hero = 'batman';
+
+    service.schoolName = '';
+    list = [];
+
+    service.getHero = function() {
+      return hero;
+    }
+
+    service.submitSchool = function(schoolName) {
+      service.schoolName = schoolName;
+      if (service.schoolName) {
+        list.push(service.schoolName);
+        service.schoolName = '';
       }
     }
 
+    service.getSchools = function() {
+      return list;
+    }
+    return service;
+  })
+  .controller('addSchoolsController', function($scope, $state, schoolFactory){
+    $scope.schoolName = '';
+    $scope.updateList = function() {
+      schoolFactory.submitSchool($scope.schoolName);
+    };
+    $scope.list = schoolFactory.getSchools();
   });
+
+
+  // $state.go('favorites');
--- client/app/schools/schools.js
+++ client/app/schools/schools.js
@@ -1,17 +1,16 @@
 angular.module('skool.schools', [])
 
-.controller('schoolsController', function ($scope, Links) {
+.controller('schoolsController', function ($scope) {
   $scope.data = {};
-
   $scope.ally = 'Ahmad Ally';
-
-  Schools.getAll()
-    .then(function (schools) {
-      $scope.data.schools = schools;
-    })
-    .catch(function(error){
-      console.error(error);
-    });
-
-
 });
+
+
+
+ // Schools.getAll()
+ //    .then(function (schools) {
+ //      $scope.data.schools = schools;
+ //    })
+ //    .catch(function(error){
+ //      console.error(error);
+ //    });
